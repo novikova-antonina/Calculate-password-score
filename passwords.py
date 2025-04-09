119cdbc5c9025dbbe60d6f0d9a3d00139cf4de42
Antonina Novikova <novikovantoninalex@gmail.com>
2025-04-09 15:14:58 +0300

    Add changes
    Код структурирован. Обновлены наименования функций, использован методы распознавания спец. символов Python

--- passwords.py
+++ passwords.py
@@ -1,15 +1,19 @@
-PASSWORD = input("Введите пароль:")
-
-
-def calculate_password_score(PASSWORD):
-    special_symbols = "@#$%^&*()-_=+[]{}|;:',.<>/?!"
+def calculate_password_score(password):
     return (
-        (len(PASSWORD) > 12) * 2
-        + any(char.isdigit() for char in PASSWORD) * 2
-        + any(char.isupper() for char in PASSWORD) * 2
-        + any(char.islower() for char in PASSWORD) * 2
-        + any(char in special_symbols for char in PASSWORD) * 2
+        (len(password) > 12) * 2
+        + any(char.isdigit() for char in password) * 2
+        + any(char.isupper() for char in password) * 2
+        + any(char.islower() for char in password) * 2
+        + any(not char.isalnum() and not char.isspace for char in password) * 2
     )
 
 
-print(f"Рейтинг пароля: {calculate_password_score(PASSWORD)}")
+def main():
+    password = input("Введите пароль:")
+    score = calculate_password_score(password)
+    print(f"Рейтинг пароля: {score}")
+
+
+if __name__ == "__main__":
+
+    main()
